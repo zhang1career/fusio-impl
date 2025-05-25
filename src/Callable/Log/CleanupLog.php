963cd921a8dd204c1f5a77844054875970ab738a
zhangrj <rongjin.zh@gmail.com>
2025-05-25 17:08:30 +0800

    debug for log cleanup supported

--- src/Callable/Log/CleanupLog.php
+++ src/Callable/Log/CleanupLog.php
@@ -2,10 +2,14 @@
 
 namespace Fusio\Impl\Callable\Log;
 
-use Fusio\Impl\Service\Log;
+use Fusio\Engine\ContextInterface;
+use Fusio\Engine\ParametersInterface;
+use Fusio\Engine\RequestInterface;
+use Fusio\Impl\Callable\BaseCall;
 use Fusio\Impl\Service\ConfigService;
+use Fusio\Impl\Service\Log;
 
-class CleanupLog
+class CleanupLog extends BaseCall
 {
     private Log $logService;
 
@@ -14,9 +18,13 @@ class CleanupLog
         $this->logService = $logService;
     }
 
-    public function invoke()
+    public function handle(RequestInterface $request, ParametersInterface $configuration, ContextInterface $context): mixed
     {
         $logExpire = ConfigService::enval('LOG_EXPIRE_IN_DAYS', 30);
         $this->logService->cleanup($logExpire);
+        return [
+            'success' => true,
+            'message' => 'Log entries older than ' . $logExpire . ' days have been removed',
+        ];
     }
 }
