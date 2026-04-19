f0d8613a406d8c8757bfa2b2a7a9ebdd8c0b025b
zhangrj <zhang_career@foxmail.com>
2026-04-19 21:00:39 +0800

    usability added

--- src/Service/System/FrameworkConfig.php
+++ src/Service/System/FrameworkConfig.php
@@ -102,7 +102,7 @@ class FrameworkConfig
      */
     public function getUserCenterBaseUrl(): string
     {
-        $url = $this->config->get('ext_user_center_url');
+        $url = $this->config->get('fusio_user_center_url');
         if (!is_string($url) || $url === '') {
             return '';
         }
@@ -115,7 +115,7 @@ class FrameworkConfig
      */
     public function getUserCenterMePath(): string
     {
-        $path = $this->config->get('ext_user_center_me_path');
+        $path = $this->config->get('fusio_user_center_me_path');
         if (!is_string($path) || $path === '') {
             return '/api/user/me';
         }
